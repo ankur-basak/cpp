b2c5f8eb80bccbbbf93dc2fbdabb5fb8b5cee360
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2024-11-09 11:03:53 +0600

    default_constractor

---
+++ practice-function/default_constractor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class student
+{
+    public:
+        int id;
+        float gpa;
+
+        void Display()
+        {
+            cout<< "Student id is:  "<< id << " and his gpa: "<< gpa<<endl;
+        }
+
+        student (int x, float y)
+        {
+           id = x;
+           gpa = y;
+        }
+        student()
+        {
+            cout<<"Default Constractor"<<endl;
+        }
+};
+
+int main(){
+
+    student Arnab;
+
+    student Ankur (1127, 3.52);
+    Ankur.Display();
+
+
+    student Akash (1128, 3.75);
+    Akash.Display();
+
+    getch();
+}
+
