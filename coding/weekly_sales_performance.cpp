24b3bd5bb7031301fd16a6e2bca5fb21ce15f269
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2024-11-09 11:12:43 +0600

    weekly_sales_performance
    These codes were practiced in class

---
+++ coding/weekly_sales_performance.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+    float salesDay[7];
+    float salesPerDay = 0;
+    double weeklyTotalSale = 0,avg;
+    int countOfDay = 0;
+
+        cout<<"Please input 7 Days of Sales\n"<<endl;
+
+    for(int i=0; i<7; i++){
+        cout<<"Sales of day "<<i<<": ";
+        cin>>salesDay[i];
+    }
+
+    cout<<"\nPerformance of company: \n"<<endl;
+
+    for(int i=0; i<7; i++){
+        cout<<"Sales of day "<<i<<": "<<salesDay[i]<<endl;
+
+        weeklyTotalSale = weeklyTotalSale + salesDay[i];
+    }
+
+    cout<<"Total Weekly sale: "<<weeklyTotalSale<<endl;
+    avg = weeklyTotalSale/7;
+    cout<<"Avg Sell: "<<avg<<endl;
+
+    for(int i=0; i<7; i++){
+
+        if(avg>salesDay[i]){
+            countOfDay += 1;
+        }
+    }
+        cout<<"Count Of Day Below Average: "<<countOfDay<<endl;
+
+    if(weeklyTotalSale>=10000){
+        cout<<"Weekly Total Target Achieved"<<endl;
+    }
+    else{
+        cout<<"Weekly Total Target Not Achieved";
+    }
+
+    getch();
+}
